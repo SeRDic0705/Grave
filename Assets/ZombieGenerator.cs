fa1fc6079a2ccd830fbe20eb5cde0dcbc36c9087
WG-SOL <32182825@dankook.ac.kr>
2024-12-01 04:27:00 +0900

    monster added

---
+++ Assets/ZombieGenerator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieGenerator : MonoBehaviour
+{
+    public GameObject zombiePrefab; 
+    public float spawnRadius = 10f; // spawn 위치
+    public int maxZombies = 5; // max 
+    public float spawnInterval = 3f; // spawn 간격(second)
+
+    private float spawnTimer = 0f; // timer
+
+    private void Update()
+    {
+        // 일정 시간 간격으로 몬스터를 생성
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnInterval && maxZombies > 0)
+        {
+            spawnTimer = 0f;
+            SpawnZombie();
+        }
+    }
+
+    private void SpawnZombie()
+    {
+        // spawn loc
+        Vector3 spawnPosition = new Vector3(
+            transform.position.x + Random.Range(-spawnRadius, spawnRadius),
+            transform.position.y,
+            transform.position.z + Random.Range(-spawnRadius, spawnRadius)
+        );
+
+        // prefab 생성
+        Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
+
+        // max test
+        maxZombies--;
+    }
+}
